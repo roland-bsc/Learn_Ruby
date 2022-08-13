1c06b2ba7006f8c953fb357d14f0ddf433c5448a
roland-bsc <rabellaatbsc@gmail.com>
2022-08-13 16:32:00 -0700

    new commit

--- iterations.rb
+++ iterations.rb
@@ -10,3 +10,51 @@ family = { "Homer" => "dad",
 
 friends.each { |x| puts "#{x}" }
 family.each { |x, y| puts "#{x}: #{y}" }
+
+# This is how you ieterate through arrays
+# array.each { |element| action }
+
+## Iterations
+# languages = ["HTML", "CSS", "JavaScript", "Python", "Ruby"]
+
+# fruits = ["grapes", "starfruit", "duhat", "cayomito", "strawberry"]
+
+# puts "Langauges: "
+# languages.each { |element| puts element}
+# puts "Fruits: "
+# fruits.each { |element| puts element}
+
+
+## Interation over multi dimensional arrays
+# s = [["ham", "swiss"], ["turkey", "cheddar"], ["roast beef", "gruyere"]]
+
+# s.each do | sub_array |
+#   sub_array.each do | y |
+#     puts y
+#   end
+#   puts "---" #separator between arrays
+# end
+
+##Iterating through hash
+# secret_identities = {
+#     "The Batman" => "Bruce Wayne",
+#     "Superman" => "Clark Kent",
+#     "Wonder Woman" => "Diana Prince",
+#     "Freakazoid" => "Dexter Douglas"
+# }
+    
+# secret_identities.each do |title, real_name|
+#     puts "#{title}: #{real_name}"
+# end
+
+#Anothder hash example
+#printing just the ORDER
+# lunch_order = {
+#     "Ryan" => "wonton soup",
+#     "Eric" => "hamburger",
+#     "Jimmy" => "sandwich",
+#     "Sasha" => "salad",
+#     "Cole" => "taco"
+#   }
+  
+#   lunch_order.each { |x, y| puts "#{y}" }
