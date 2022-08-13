122462d2b5de8da6b21415ba973c0ea11e52b68b
roland-bsc <rabellaatbsc@gmail.com>
2022-08-13 12:50:10 -0700

    new commit

--- for_loop.rb
+++ for_loop.rb
@@ -1,6 +1,25 @@
 i = 20
 loop do
   i -= 1
-  print "#{i}"
+  next if i % 2 != 0
+   print "#{i}"
   break if i <= 0
 end
+
+# i = 20
+# loop do
+#   i -= 1
+#   print "#{i}"
+#   break if i <= 0
+# end
+
+
+# from 1 - 20 with two dots
+# for num in 1..20
+#     puts num
+# end
+
+# from 1 to 19
+# for num in 1...20
+#     puts num
+# end
