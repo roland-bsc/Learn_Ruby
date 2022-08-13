8ec215dbd79a99316636a10f3f76e80b3d3c0707
roland-bsc <rabellaatbsc@gmail.com>
2022-08-13 15:29:15 -0700

    array commit

--- array.rb
+++ array.rb
@@ -13,3 +13,9 @@ end
 # odds.each do |item|
 #   print item*2
 # end
+
+# # two dimensional array
+# my_2d_array = [["he", "them", "they"], [2, 3, 5, 6, 12]]
+
+# puts my_2d_array[1]
+# puts my_2d_array[0]
