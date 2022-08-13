093bc8f07dad456438ba476a328ede05f4f808fe
roland-bsc <rabellaatbsc@gmail.com>
2022-08-13 15:46:26 -0700

    new file "hash"

---
+++ hash.rb
@@ -0,0 +1,15 @@
+my_hash = { "name" => "Eric",
+    "age" => 26,
+    "hungry?" => true
+  }
+  
+  puts my_hash["name"]
+  puts my_hash["age"]
+  puts my_hash["hungry?"]
+
+
+# # One way to create a Hash
+# pets = Hash.new
+# pets["Stevie"] = "cat"
+# pets["George"] = "dog"
+# pets["Roman"] = "eel"
