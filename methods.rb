3f4996f20d78108fc5ccbe89d705a77773b41818
roland-bsc <rabellaatbsc@gmail.com>
2022-08-28 13:05:27 -0700

    new file

--- methods.rb
+++ methods.rb
@@ -57,3 +57,14 @@ cubertino(8) #calling cubertino method
 #       return false
 #   end
 # end
+
+# # method that capitalizes a word
+# def capitalize(string) 
+#   puts "#{string[0].upcase}#{string[1..-1]}"
+# end
+
+# capitalize("ryan") # prints "Ryan"
+# capitalize("jane") # prints "Jane"
+
+# # block that capitalizes each string in the array
+# ["ryan", "jane"].each {|string| puts "#{string[0].upcase}#{string[1..-1]}"} # prints "Ryan", then "Jane"
