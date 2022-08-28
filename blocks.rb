2b08463172330ae2c47031e2d8e2a40b40c77c11
roland-bsc <rabellaatbsc@gmail.com>
2022-08-28 13:18:02 -0700

    more commit

--- blocks.rb
+++ blocks.rb
@@ -1,3 +1,18 @@
+=begin
+Using Code Blocks
+A method can take a block as a parameter. That’s what .each has been doing this whole time: 
+taking a block as a parameter and doing stuff with it! You just didn’t notice because 
+we didn’t use the optional parentheses. We are sneaky.
+
+Passing a block to a method is a great way of abstracting certain tasks from the method 
+and defining those tasks when we call the method. Abstraction is an important idea in computer science, 
+and you can think of it as meaning “making something simpler.” 
+Imagine if when you wanted to house hunt, you had to say, 
+“Honey, let’s go look at configurations of concrete, plywood, and vinyl siding.” 
+That’d be crazy! Just like saying “house” simplifies listing its components, 
+using a block to define the task you want the method (like .each) to do simplifies the task at hand.
+=end
+
 # The block, {|i| puts i}, is passed the current
 # array item each time it is evaluated. This block
 # prints the item. 
--- sort_stuff.rb
+++ sort_stuff.rb
@@ -1,4 +1,24 @@
 databases = ["Elasticsearch", "Memcached,", "MongoDB,", "MySQL,", "PostgreSQL,", "Redis"]
 
-print databases.sort_by {|content| content.length}
+print databases.sort_by {|content| content.length} #sorts by array element string length
 puts " "
+
+
+my_array = [3, 4, 8, 7, 1, 6, 5, 9, 2]
+
+# Call the sort! method on my_array below.
+# my_array should then equal [1, 2, 3, 4, 5, 6, 7, 8, 9].
+
+my_array.sort!
+
+print my_array
+
+# more sorting
+# library sorting code
+books = ["Charlie and the Chocolate Factory", "War and Peace", "Utopia", "A Brief History of Time", "A Wrinkle in Time"]
+
+# How might we sort! the books in alphabetical order? (Hint, hint)
+
+books.sort!
+
+books.each { |books| puts books} #takes each argument from books array and prints it in it's own line
