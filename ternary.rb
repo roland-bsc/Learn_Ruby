3ed41dc900d9262bebf8651927b67185db27517e
roland-bsc <rabellaatbsc@gmail.com>
2022-09-04 12:59:01 -0700

    new stuff

---
+++ ternary.rb
@@ -0,0 +1,11 @@
+# boolean ? Do this if true: Do this if false
+
+
+# puts "Put a number for variable 'a': "
+# a = gets.chomp.to_i
+# puts "Put a number for variable 'b': "
+# b = gets.chomp.to_i
+
+# puts a > b ? "A is greater than B": "B is greater tha A"
+
+# puts "A is #{a} and B is #{b}."
---
+++ when_then.rb
@@ -0,0 +1,19 @@
+case language
+  when "JS" then puts "Websites!"
+  when "Python" then puts "Science!"
+  when "Ruby" then puts "Web apps!"
+  else puts "I don't know!"
+end
+
+# # IS THE SAME AS 
+
+# case language
+# when "JS"
+#   puts "Websites!"
+# when "Python"
+#   puts "Science!"
+# when "Ruby"
+#   puts "Web apps!"
+# else
+#   puts "I don't know!"
+# end
