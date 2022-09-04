a405abef91a381a861cf79f44e6e33279ed50494
roland-bsc <rabellaatbsc@gmail.com>
2022-09-04 13:14:09 -0700

    whenthen

--- when_then.rb
+++ when_then.rb
@@ -17,3 +17,19 @@ end
 # else
 #   puts "I don't know!"
 # end
+
+
+##SAMPLE
+# puts "Hello there!"
+# puts "Pick a language: English, French, German, Finnish"
+# greeting = gets.chomp.downcase
+
+
+# # Add your case statement below!
+# case greeting
+#   when "English" then puts "Hello!"
+#   when "French" then puts "Bonjour!"
+#   when "German" then puts "Guten tag!"
+#   when "Finnish" then puts "Haloo!"
+#   else puts "I don't know that language!"
+# end
